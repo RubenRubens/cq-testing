532f2440c512449e91c2d110a9f65a8f38bf5640
Ruben A <a28144415@gmail.com>
2021-01-12 12:28:39 +0100

    Add DXF extrusion test

---
+++ src/dxf_extrusion/A2.py
@@ -0,0 +1,12 @@
+# issue https://github.com/CadQuery/cadquery/issues/515
+
+import cadquery as cq
+
+result = (
+    cq.importers.importDXF('src/dxf_extrusion/test.dxf')
+    .wires().toPending()
+    .offset2D(2,kind='intersection')
+    .extrude(10)
+    .faces('>Z')
+    .chamfer(1)
+)
