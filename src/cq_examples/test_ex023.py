444ebc0d21dc368ec24e38bf2d5a3dce9f9f68b0
Ruben A <a28144415@gmail.com>
2021-01-24 14:05:36 +0100

    Includes example 23 on passing

--- src/cq_examples/test_ex023.py
+++ src/cq_examples/test_ex023.py
@@ -4,5 +4,8 @@ import src.cq_examples.Ex023_Sweep as ex
 class TestExample023(unittest.TestCase):
     def test_Ex023(self):
         # Import and validate
-        R = ex.result
-        self.assertTrue(R.val().isValid())
+        self.assertTrue(ex.a.val().isValid())
+        self.assertTrue(ex.b.val().isValid())
+        self.assertTrue(ex.c.val().isValid())
+        self.assertTrue(ex.d.val().isValid())
+        self.assertTrue(ex.e.val().isValid())
