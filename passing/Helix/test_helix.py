a871fe51a2b9df5ef33b07cbc29644b2f11c42ca
Ruben A <ruben.arsi@protonmail.ch>
2021-05-08 13:57:51 +0200

    Change Helix structure

--- passing/Helix/test_helix.py
+++ passing/Helix/test_helix.py
@@ -1,23 +1,59 @@
+import cadquery as cq
 import unittest
+from math import pi, cos, sin
+
+
+def A():
+    r = 3 # Radius of the helix
+    p = 1.5 # Pitch of the helix
+    h = 4 # Height of the helix
+
+    # Helix
+    wire = cq.Wire.makeHelix(pitch=p, height=h, radius=r)
+    helix = cq.Workplane(obj=wire)
+
+    # Final result. A circle sweeped along a helix.
+    result = (
+        cq.Workplane('XZ')
+        .center(r, 0)
+        .circle(0.1)
+        .sweep(helix, isFrenet=True)
+    )
+
+    return result
+
+
+def B():
+    r = 3 # Radius of the helix
+    p = 1.5 # Pitch of the helix
+    h = 4 # Height of the helix
+
+    # Helix
+    helix = cq.Workplane('XY').parametricCurve(
+        lambda t : (
+            r * cos(t * 2 * pi),
+            r * sin(t * 2 * pi),
+            p * t
+        ),
+        start=0,
+        stop=h/p
+    )
+
+    # Final result. A circle sweeped along a helix.
+    result = (
+        cq.Workplane('XZ')
+        .center(r, 0)
+        .circle(0.1)
+        .sweep(helix, isFrenet=True)
+    )
+
+    return result
+
 
 class TestHelix(unittest.TestCase):
-
-    # Import and validate H1
-    def test_H1(self):
-        import Helix.H1
-        global R1
-        R1 = Helix.H1.result
-        self.assertTrue(R1.val().isValid())
-
-    # Import and validate H2
-    def test_H2(self):
-        import Helix.H2
-        global R2
-        R2 = Helix.H2.result
-        self.assertTrue(R2.val().isValid())
-
-    # Tests if the objects are equal
-    def test_equal(self):
+    def test_volumes(self):
+        R1 = A()
+        R2 = B()
         self.assertAlmostEqual(R1.cut(R2).val().Volume(), 0, places=4)
         self.assertAlmostEqual(R1.union(R2).val().Volume(), R1.val().Volume(), places=4)
         self.assertAlmostEqual(R1.intersect(R2).val().Volume(), R1.val().Volume(), places=4)
